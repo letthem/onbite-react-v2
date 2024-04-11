1bab32fcb81024c9c93d4f20811168e03d146559
letthem <suyeon3662@naver.com>
2024-04-12 00:22:28 +0900

    feat: Node.js

---
+++ 03. Node.js/src/index.js
@@ -0,0 +1,9 @@
+import mul, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2)); // 3
+// console.log(sub(1, 2)); // -1
+// console.log(mul(2, 3)); // 6
