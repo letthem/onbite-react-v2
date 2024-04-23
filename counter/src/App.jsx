fee7c8aaaa3164218d136c0809de6e5da6896bbf
letthem <suyeon3662@naver.com>
2024-04-23 22:29:47 +0900

    feat: useEffect로 라이프사이클 제어하기

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,15 +1,30 @@
 import "./App.css";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
 import Viewer from "./components/Viewer";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
+  const isMount = useRef(false); // 초기값 : 아직 mount되지 않았다
+
+  // 1. 마운트 : 탄생
   useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
-  // 의존성 배열 = dependency array = deps
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      // 아직 mount되지 않았으면
+      isMount.current = true; // mount 시키고
+      return; // update 출력하지 않고 강제 종료
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트 : 죽음
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -28,6 +43,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
