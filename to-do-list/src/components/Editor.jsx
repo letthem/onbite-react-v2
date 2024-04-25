9d08593815e4773d202240cb71cef93fd9d9a2f4
letthem <suyeon3662@naver.com>
2024-04-25 23:43:45 +0900

    feat: Create - 투두 추가하기

--- to-do-list/src/components/Editor.jsx
+++ to-do-list/src/components/Editor.jsx
@@ -1,10 +1,39 @@
+import { useRef, useState } from "react";
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus(); // 빈 문자열 추가시 focus & 함수 나가기
+      return;
+    }
+    onCreate(content); // 새로운 todo 추가
+    setContent(""); // 빈 문자열로 state 초기화
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) { // enter 누를 시 제출
+      onSubmit();
+    }
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
