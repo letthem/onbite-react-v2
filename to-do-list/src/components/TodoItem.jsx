3790377612d3bd33eb9491aab13cb504b2b8d6a2
letthem <suyeon3662@naver.com>
2024-04-30 08:54:13 +0900

    Refactor: React.memo와 컴포넌트 렌더링 최적화

--- to-do-list/src/components/TodoItem.jsx
+++ to-do-list/src/components/TodoItem.jsx
@@ -1,5 +1,5 @@
 import "./TodoItem.css";
-
+import { memo } from "react";
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   const onChangeCheckbox = () => {
     onUpdate(id);
@@ -8,7 +8,7 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   const onClickDeleteButton = () => {
     onDelete(id);
   };
-  
+
   return (
     <div className="TodoItem">
       <input
@@ -24,4 +24,16 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+// 고차 컴포넌트 (HOC) : 컴포넌트 호출만으로도 새로운 기능 부여 가능
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라, Props가 바뀌었는지 안 바뀌었는지 판단
+  // T -> Props 바뀌지 않음 -> 리렌더링 X
+  // F -> Props 바뀜 -> 리렌더링 O
+
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
